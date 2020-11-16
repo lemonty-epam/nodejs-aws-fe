5cb633d9f8974016c96d879e435a2344277eb5c7
Irina Savitskaya <irina_savitskaya1@epam.com>
2020-11-16 17:57:04 +0300

    task 5 - integrate import function with backend call

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://5odk5h4739.execute-api.eu-central-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://2piqqikyn3.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
